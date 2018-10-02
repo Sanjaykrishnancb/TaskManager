a492a02e7582ef76e121cf6d291baa5110990878
sanjaykrishnancb <sanjaykrishnan1994@gmail.com>
2018-10-02 20:42:47 +0530

    open Cover testing

---
+++ ProjectManager.Test/openCover.bat
@@ -0,0 +1,3 @@
+..\..\..\packages\OpenCover.4.6.519\tools\OpenCover.Console.exe -target:..\..\..\ProjectManager.Test\bin\Debug\runTests.bat -register:user -filter:+[ProjectManager.BusinessLayer,ProjectManager.Service]* 
+
+pause
